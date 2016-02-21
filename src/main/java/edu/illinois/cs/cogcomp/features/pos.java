aefbf458adf4cfad0e764e95d66559945e5c0f9f
Nitish Gupta <gnnitish@gmail.com>
2016-02-21 15:11:51 -0600

    NLP Pileline-0.1.16 for POS and Shallow. Reader is working for test file. POS and Shallow from Pipleline is working.TODO  : For ACE04 and ACE05 compile filelists for train, val and test separately and write code to read all docs of different sets.

---
+++ src/main/java/edu/illinois/cs/cogcomp/features/pos.java
@@ -0,0 +1,74 @@
+package edu.illinois.cs.cogcomp.features;
+
+import edu.illinois.cs.cogcomp.annotation.AnnotatorService;
+import edu.illinois.cs.cogcomp.annotation.AnnotatorServiceConfigurator;
+import edu.illinois.cs.cogcomp.config.PipelineConfig;
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
+import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
+import edu.illinois.cs.cogcomp.nlp.common.PipelineConfigurator;
+import edu.illinois.cs.cogcomp.nlp.pipeline.IllinoisPipelineFactory;
+import edu.illinois.cs.cogcomp.nlp.util.SimpleCachingPipeline;
+
+/**
+ * Created by nitishgupta on 2/21/16.
+ */
+public class pos {
+
+    private static AnnotatorService prep;
+
+    public static void addShallowParse(TextAnnotation ta) {
+        ResourceManager pipelineRm = new PipelineConfig().getDefaultConfig();
+        ResourceManager annotatorServiceRm = new AnnotatorServiceConfigurator().getConfig(pipelineRm);
+
+        try {
+            prep = (SimpleCachingPipeline) IllinoisPipelineFactory.buildPipeline(annotatorServiceRm);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+        try{
+            prep.addView(ta, ViewNames.SHALLOW_PARSE);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+    }
+
+
+
+    public static void addPOS(TextAnnotation ta) {
+        ResourceManager pipelineRm = new PipelineConfig().getDefaultConfig();
+        ResourceManager annotatorServiceRm = new AnnotatorServiceConfigurator().getConfig(pipelineRm);
+
+        try {
+            prep = (SimpleCachingPipeline) IllinoisPipelineFactory.buildPipeline(annotatorServiceRm);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+        try{
+            prep.addView(ta, ViewNames.POS);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
