620777b8aa04ff33e193df37472cec901f044d8f
Nitish Gupta <gnnitish@gmail.com>
2016-03-31 22:29:59 -0500

    Reads docs and creates Single TA for whole text. Adds a Span Label NER View that contains the mention extents Adds NER BIO View from the above view Baseline NER runs fine
    ACE2005 - Now able to read almost all of NW and BN (329 / 331)

--- src/main/java/edu/illinois/cs/cogcomp/erc/ir/Document.java
+++ src/main/java/edu/illinois/cs/cogcomp/erc/ir/Document.java
@@ -1,14 +1,21 @@
 package edu.illinois.cs.cogcomp.erc.ir;
 
+import edu.illinois.cs.cogcomp.annotation.TextAnnotationBuilder;
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TokenLabelView;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.View;
 import edu.illinois.cs.cogcomp.erc.corpus.Corpus;
 import edu.illinois.cs.cogcomp.erc.util.PipelineService;
+import edu.illinois.cs.cogcomp.nlp.tokenizer.IllinoisTokenizer;
+import edu.illinois.cs.cogcomp.nlp.utility.CcgTextAnnotationBuilder;
 import edu.illinois.cs.cogcomp.reader.ace2005.annotationStructure.ACEDocumentAnnotation;
+import edu.illinois.cs.cogcomp.reader.ace2005.annotationStructure.ACEEntity;
+import edu.illinois.cs.cogcomp.reader.ace2005.annotationStructure.ACEEntityMention;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Bhargav Mangipudi on 2/26/16.
@@ -18,12 +25,31 @@ public class Document implements Serializable {
 
     TextAnnotation ta;
     ACEDocumentAnnotation aceAnnotation;
+    String contentRemovingTags;
     boolean is2004;
     String filename;
 
-    public Document(TextAnnotation ta, ACEDocumentAnnotation aceAnnotation, boolean is2004, String filename) {
+
+
+    public Document(TextAnnotation ta, ACEDocumentAnnotation aceAnnotation, String contentRemovingTags, boolean is2004, String filename) {
         this.ta = ta;
         this.aceAnnotation = aceAnnotation;
+        this.contentRemovingTags = contentRemovingTags;
+        this.is2004 = is2004;
+        this.filename = filename;
+
+        // Add Pipeline Views as well
+        this.addPipeLineViews();
+    }
+
+    public Document(ACEDocumentAnnotation aceAnnotation, String contentRemovingTags, boolean is2004, String filename) {
+        TextAnnotationBuilder taBuilder = new CcgTextAnnotationBuilder(new IllinoisTokenizer());
+        if(is2004)
+            ta = taBuilder.createTextAnnotation( "ACE2004", filename, contentRemovingTags);
+        else
+            ta = taBuilder.createTextAnnotation( "ACE2005", filename, contentRemovingTags);
+        this.aceAnnotation = aceAnnotation;
+        this.contentRemovingTags = contentRemovingTags;
         this.is2004 = is2004;
         this.filename = filename;
 
@@ -62,4 +88,15 @@ public class Document implements Serializable {
         return null;
     }
 
+    public String getContentRemovingTags(){ return contentRemovingTags; }
+
+    public List<ACEEntityMention> getGoldMentions(){
+        List<ACEEntityMention> mentions = new ArrayList<ACEEntityMention>();
+        List<ACEEntity> entityList = aceAnnotation.entityList;
+
+        for(ACEEntity entity : entityList){
+            mentions.addAll(entity.entityMentionList);
+        }
+        return mentions;
+    }
 }
