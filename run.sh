9c8a6d1e469ff54213c1e0a105eb56e734654050
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2016-04-10 15:38:18 -0500

    Update run scripts to use `mvn exec`. Exclude older edison package.

--- run.sh
+++ run.sh
@@ -1,13 +1,4 @@
 set -e
 
-#mvn clean
-#mvn dependency:copy-dependencies
+mvn -q compile exec:java -Dexec.args="-Xmx8g" -Dexec.mainClass=edu.illinois.cs.cogcomp.erc.main.Main -Dexec.args"$*"
 
-mvn compile -q
-
-DEFAULT_PACKAGE="edu.illinois.cs.cogcomp.erc"
-PACKAGE="main"
-MAINCLASS="Main"
-CP="./:./target/classes/:./target/dependency/*:./config/:target/dependency/*"
-
-java -Xmx8g -cp $CP $DEFAULT_PACKAGE.$PACKAGE.$MAINCLASS $*
